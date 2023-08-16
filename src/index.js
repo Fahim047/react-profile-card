693a938c06ca46cb165db7bf3cfb7aa139e07610
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2023-08-16 13:45:56 +0000

    conditional emoji rendering

--- src/index.js
+++ src/index.js
@@ -73,11 +73,15 @@ function SkillList() {
   );
 }
 
-function Skill(props) {
+function Skill({ skill, color, level }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "😒"}
+        {level === "intermediate" && "😎"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
